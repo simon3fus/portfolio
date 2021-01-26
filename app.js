b469932dc4c81be9469080c8dce678c4c46c54ce
simon3fus <71319069+simon3fus@users.noreply.github.com>
2021-01-26 15:58:00 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const hero = document.querySelector(".hero");
 const headline = document.querySelector("#headline");
 
 const tl = new TimelineMax();
-tl.fromTo (hero, 1.5, {height: "0%"}, {height: "90%", ease: Power2.easeInOut})
-.fromTo (hero, 1.8, {width: "100%"}, {width:"80%", ease: Power2.easeInOut}, "-=0.0")
+tl.fromTo (hero, 1.5, {height: "0%"}, {height: "100%", ease: Power2.easeInOut})
+.fromTo (hero, 1.8, {width: "0%"}, {width:"100%", ease: Power2.easeInOut}, "-=1.5")
 .fromTo (headline, 1.2, {x: "50%", opacity:"0"}, {x:"0%",opacity:"1", ease: Power2.easeInOut}, "-=0.8");
